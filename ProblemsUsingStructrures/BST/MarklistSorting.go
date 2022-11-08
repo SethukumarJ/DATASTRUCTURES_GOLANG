01ceaffc07e826f45bfc0d61f8b10a506240eab8
Sethukumar J <sethukumarj.76@gmail.com>
2022-11-08 23:19:49 +0530

    remove added

--- ProblemsUsingStructrures/BST/MarklistSorting.go
+++ ProblemsUsingStructrures/BST/MarklistSorting.go
@@ -33,6 +33,7 @@ func (tree *ProgressCard) addNode() {
 
 	}
 
+
 	temp := tree.root
 
 	for temp != nil {
@@ -67,6 +68,115 @@ func (tree *ProgressCard) addNode() {
 }
 
 
+func (tree *ProgressCard) delete() {
+
+	var name string
+	fmt.Print("Enter the name of student to remove : ")
+	fmt.Scan(&name)
+	tree.deletehelper(tree.root, name)
+}
+
+
+func (tree *ProgressCard) deletehelper(temp *Node,name string) {
+
+	
+
+	for temp != nil {
+
+		if temp.name == name {
+
+			tree.delete(temp.mark)
+			return
+
+		} 
+
+			tree.deletehelper(temp.lchild,name)
+			tree.deletehelper(temp.rchild,name)
+		
+
+	}
+
+}
+
+func (tree *BinarySearchTree) remove () {
+
+	var data int
+	temp := tree.root
+
+	fmt.Print("\nEnter the data to remove : ")
+	fmt.Scan(&data)
+
+	if temp == nil {
+
+		fmt.Println("Tree is empty !...")
+		return
+
+	}
+
+	for temp != nil {
+
+		if data < temp.Data {
+
+			temp = temp.left
+
+		} else if data > temp.Data {
+
+			temp = temp.right
+
+		} else {
+
+				if temp.right!=nil {
+
+                	temp.Data = getMin(temp)
+
+				} else if temp.left!=nil {
+
+                	temp.Data = getMax(temp)
+
+				} else {
+
+                	temp.Data = getMin(temp)
+                	fmt.Println("Data deleted succesfully")
+                	return
+
+				}
+			}
+		}
+
+	}
+
+
+
+	func getMax(key *Node) int {
+        prev := key
+        temp := key.lchild
+        for temp.rchild != nil {
+            prev = temp
+            temp = temp.rchild
+        }
+        if prev == key {
+            prev.lchild = temp.lchild
+        } else {
+            prev.rchild = temp.lchild
+        }
+        return temp.mark
+    }
+
+	func getMin(key *Node) int {
+        prev := key
+        temp := key.rchild
+        for temp.lchild != nil {
+            prev = temp
+            temp = temp.lchild
+        }
+        if prev == key {
+            prev.rchild = temp.rchild
+        } else {
+            prev.lchild = temp.rchild
+        }
+        return temp.mark
+    }
+
 func (tree *ProgressCard) inOrder () {
 
 	fmt.Println("=======RANK LIST========")
@@ -85,6 +195,9 @@ func (tree *ProgressCard) inOrderHelper (temp *Node) {
 	}
 
 }
+
+
+
 
 
 
