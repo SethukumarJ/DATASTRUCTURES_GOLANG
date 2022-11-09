20256ed7262adb8e88ec152635024767b6bd95a1
Sethukumar J <sethukumarj.76@gmail.com>
2022-11-09 22:40:07 +0530

    nearest

---
+++ ProblemsUsingStructrures/BST/findNearest.go
@@ -0,0 +1,167 @@
+package main
+
+
+import (
+	"fmt"
+)
+
+
+type Node struct {
+
+	Data int
+	right *Node
+	left *Node
+	parent *Node
+
+}
+
+
+type BinarySearchTree struct {
+	
+	root *Node
+
+}
+
+
+func (tree *BinarySearchTree) addNode () {
+
+	var data int
+	newNode := new(Node)
+
+	fmt.Print("\nEnter the data to add :")
+	fmt.Scan(&data)
+
+	newNode.Data = data
+
+	if (tree.root == nil) {
+		
+		tree.root = newNode
+		return
+
+	}
+
+	temp := tree.root
+
+	for temp != nil {
+
+		if newNode.Data < temp.Data {
+
+			if temp.left == nil {
+
+				temp.left = newNode
+				newNode.parent = temp
+				return
+			}
+
+			temp = temp.left
+
+		} else if newNode.Data > temp.Data {
+
+			if temp.right == nil {
+
+				temp.right = newNode
+				newNode.parent = temp
+				return
+
+			}
+
+			temp = temp.right
+
+		}
+
+	}
+
+}
+
+
+func (tree *BinarySearchTree) contain() {
+
+	var data int
+	temp := tree.root
+
+	fmt.Print("\nEnter the data to check whether the tree contains or not : ")
+	fmt.Scan(&data)
+
+	if temp == nil {
+
+		fmt.Println("Tree is empty!...")
+		return
+	}
+
+	for temp != nil  {
+
+		if data < temp.Data {
+
+			temp = temp.left
+
+		} else if data > temp.Data {
+
+			temp = temp.right
+
+		} else {
+
+			fmt.Println("Match found!")
+			return
+
+		}
+	}
+
+	fmt.Println("No match found!")
+
+}
+
+
+func (tree *BinarySearchTree) remove () {
+
+	var data int
+	temp := tree.root
+
+	fmt.Print("\nEnter the data to remove : ")
+	fmt.Scan(&data)
+
+	if temp == nil {
+
+		fmt.Println("Tree is empty !...")
+		return
+
+	}
+
+	for temp != nil {
+
+		if data < temp.Data {
+
+			temp = temp.left
+
+		} else if data > temp.Data {
+
+			temp = temp.right
+
+		} else {
+
+				if temp.right!=nil {
+
+                	temp.Data = getMin(temp)
+
+				} else if temp.left!=nil {
+
+                	temp.Data = getMax(temp)
+
+				} else {
+
+                	temp.Data = getMin(temp)
+                	fmt.Println("Data deleted succesfully")
+                	return
+
+				}
+			}
+		}
+
+	}
+
+
+
+	func (tree *BinarySearchTree) findNearest() {
+
+		fmt.Print("Enter the data to search for the nearest : ") 
+		fmt.Scan(&data)
+	}
