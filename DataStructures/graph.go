d66d25e9b8a9ab9d0bb29532ba05fe19e4029cac
Sethukumar J <sethukumarj.76@gmail.com>
2022-11-10 23:02:13 +0530

    graph

--- DataStructures/graph.go
+++ DataStructures/graph.go
@@ -40,3 +40,45 @@ public class Graph {
     }
     
 }
+
+import java.util.*;
+public class Graph {
+    private Map<Integer,List<Integer>> map = new  HashMap<>();
+    
+    private void addVertex(Integer vertex) {
+        map.put(vertex, new ArrayList<>());
+    }
+
+    public void insert(Integer vertex, Integer edge, boolean bidirectional){
+        if(!map.containsKey(vertex)){
+            addVertex(vertex);
+
+        }
+        if(!map.containsKey(edge)){
+            addVertex(edge);
+        }
+        map.get(vertex).add(edge);
+        if(bidirectional){
+            map.get(edge).add(vertex);
+        }
+    }
+
+    public void display() {
+    
+        for(Integer x : map.keySet()){
+            System.out.print(x+": ");
+            for(Integer y: map.get(x)){
+                System.out.print(y+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.insert(3, 5, true);
+        graph.insert(3, 4, true);
+        graph.insert(5, 6, false);
+        graph.display();
+    }
+    
+}
