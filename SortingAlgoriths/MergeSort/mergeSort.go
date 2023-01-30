6dad474fb2556ef6ec226912d9a4d660eb23dad1
SethukumarJ <sethukumarj.76@gmail.com>
2023-01-30 09:51:46 +0530

    mergeSort worked properly

--- SortingAlgoriths/MergeSort/mergeSort.go
+++ SortingAlgoriths/MergeSort/mergeSort.go
@@ -1,27 +1,37 @@
 package main
 
+import "fmt"
 
-func mergeSortHelper(array []int, firstIdx int,lastIdx int) {
-
-	midIdx := (firstIdx + lastIdx)/2
-	
-
-	
-	if firstIdx == lastIdx {
-
-		return
+func mergeSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
 	}
-
-	mergeSortHelper(array , firstIdx, midIdx)
-	mergeSortHelper(array, midIdx+1, lastIdx)
-
-
+	middle := len(arr) / 2
+	left := mergeSort(arr[:middle])
+	right := mergeSort(arr[middle:])
+	return merge(left, right)
 }
 
+func merge(left, right []int) []int {
+	result := make([]int, 0, len(left)+len(right))
+	i := 0
+	j := 0
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+	return result
+}
 
-
-
-
-func mergeSort(array []int) {
-
+func main() {
+	arr := []int{5, 2, 6, 3, 1, 4}
+	arr = mergeSort(arr)
+	fmt.Println(arr)
 }
