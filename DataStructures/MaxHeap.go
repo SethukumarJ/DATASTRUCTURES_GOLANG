07d221fd8d0204d66e191ead68b2ddaa75395109
Sethukumar J <sethukumarj.76@gmail.com>
2023-02-06 17:40:35 +0530

    implemented function to convert array to maxheap

--- DataStructures/MaxHeap.go
+++ DataStructures/MaxHeap.go
@@ -8,52 +8,46 @@ import (
 
 type MaxHeap struct {
 
-	 arr [100]int
+	 arr []int
 
 }
 
-var size int = -1
+func(h *MaxHeap) insert(data int) {
 
-func (h *MaxHeap) insert(data int) {
+	h.arr = append(h.arr, data)
+	h.insertHelper(len(h.arr)-1)
 
-	h.arr[size+1] = data
-	size ++
-	h.insertHelper(size)
 }
 
 
 func (h *MaxHeap) insertHelper(position int) {
 
-	if h.arr[position] > h.arr[position - 1/2]{
-
-		swap := h.arr[(position-1)/2]
-		h.arr[(position-1)/2] = h.arr[position]
-		h.arr[position] = swap
+	if h.arr[position] > h.arr[(position-1)/2] {
+		swap(h.arr,position,(position-1)/2)
 		h.insertHelper((position-1)/2)
 	}
 }
 
-func (h *MaxHeap) rootDeleter() {
+func (h *MaxHeap) rootDelete() {
 
-	h.arr[0] = h.arr[size]
-	h.rootDeleleterHelper(0)
-	size --
-}
+	   h.arr[0] = h.arr[len(h.arr)-1]
+       h.arr = h.arr[:len(h.arr)-1]
+       h.rootDeleteHelper(0)
 
-func(h *MaxHeap) rootDeleleterHelper(position int) {
+    }
+   
+ 
+func (h *MaxHeap) rootDeleteHelper(position int) {
 
-	if h.arr[position] < h.arr[h.maxValue(position)] {
-
-		swap := h.arr[position] 
-		h.arr[h.maxValue(position)] = h.arr[position]
-		h.arr[position] = swap
-		h.rootDeleleterHelper(h.maxValue(position))
+	 if h.arr[position] < h.arr[h.minValue(position)] {
+        swap(h.arr,position,h.minValue(position))
+        h.rootDeleteHelper(h.minValue(position));
 	}
 }
 
-func (h *MaxHeap) maxValue(position int) int{
+func (h *MaxHeap) minValue(position int) int{
        
-	if(h.arr[2*position+1] < h.arr[2*position+2]){
+	if(h.arr[2*position+1] > h.arr[2*position+2]){
 
 		return 2*position+2;
 
@@ -66,20 +60,73 @@ func (h *MaxHeap) maxValue(position int) int{
 
 func (h  *MaxHeap) display() {
 
-	for i:= 0; i< size; i++ {
+	for i:= 0; i< len(h.arr); i++ {
 
 		fmt.Print(h.arr[i]," ")
 	}
+}
+
+func (h *MaxHeap) BuildHeap(array []int ) {
+
+	for i:= parent(len(h.arr)-1); i>=0; i--{
+
+		h.shiftDown(i)
+	}
+}
+
+func (h *MaxHeap) shiftDown(currentIdx int) {
+
+	endIdx := len(h.arr)-1
+	leftIdx := leftChild(currentIdx)
+
+	for leftIdx <= endIdx {
+
+		rightIdx := rightChild(currentIdx)
+		var idxToShift int 
+		if rightIdx <= endIdx && h.arr[rightIdx] > h.arr[leftIdx] {
+			idxToShift = rightIdx
+			
+		} else {
+			idxToShift = leftIdx
+		}
+
+		if h.arr[idxToShift] > h.arr[currentIdx] {
+			swap(h.arr, currentIdx,idxToShift)
+		} else {
+			return
+		}
+	}
+
+
+}
+
+func main () {
+
+heap := MaxHeap{}
+
+array := []int{10,49,3,1,5,7,12,5}
+
+heap.BuildHeap(array)
+heap.insert(9)
+heap.rootDelete()
+heap.display()
+
 }
 
 
 
 
 
+func swap(arr []int, i, j int) {
+    arr[i], arr[j] = arr[j], arr[i]
+}
 
-
-func main () {
-
-
-
+func leftChild(i int) int{
+	return (i*2) +1
+}
+func rightChild(i int) int{
+	return (i*2) +2
+}
+func parent(i int) int{
+	return(i-1)/2
 }
